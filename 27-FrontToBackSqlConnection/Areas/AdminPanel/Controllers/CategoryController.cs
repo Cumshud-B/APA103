9b21106c73bd3495f8a1fcfac3f506bda746dca7
Cumshud-B <jumshudeb-apa103@gmail.com>
2026-05-12 23:45:58 +0400

    30.Admin Panel, Category Create

---
+++ 27-FrontToBackSqlConnection/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using _27_FrontToBackSqlConnection.Data;
+using _27_FrontToBackSqlConnection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Area("AdminPanel")]
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _context.Categories.Include(c=>c.).Where(c => !c.isDeleted).ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
